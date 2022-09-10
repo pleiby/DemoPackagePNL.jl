8bf0e41e134130be795fe9126f7a8ffb2d059a42
Paul Leiby <pleiby@gmail.com>
2022-09-10 12:38:03 -0400

    print_greeting to testable form, add docstring for offer_greeting

--- src/DemoPackagePNL.jl
+++ src/DemoPackagePNL.jl
@@ -1,8 +1,29 @@
 module DemoPackagePNL
 
-# Write your package code here.
-print_greeting(xname) = print("Hello $xname, Demo world!")
+export offer_greeting
 
+# Write your package code here.
+function print_greeting(xname)
+    print("printed: Hello $xname, Demo world!")
+    return (xname)
+end
+
+"""
+    offer_greeting(xname)
+
+Return a greeting, with `xname` interpolated
+
+# Arguments
+* `xname`: string to be interpolated, usually a name
+
+# Notes
+
+# Example
+```julia
+julia> offer_greeting("Bilbo")
+"Hello Bilbo, Demo world!"
+```
+"""
 function offer_greeting(xname)
     return "Hello $xname, Demo world!"
 end
